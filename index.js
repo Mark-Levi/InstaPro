496b3f69aed736cf1db1906d9f63e3bc544a01cd
Mark-Levi <nessy-777@yandex.ru>
2023-04-13 22:52:49 +0300

    Test commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Comment
 import { getPosts } from "./api.js";
 import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
 import { renderAuthPageComponent } from "./components/auth-page-component.js";
